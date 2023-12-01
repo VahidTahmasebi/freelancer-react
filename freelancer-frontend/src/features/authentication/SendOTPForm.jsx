5bccbe9f463cf5fe8fd59818afa93bac219d2258
Vahid <vahid.tahmasebi95@gmail.com>
2023-12-01 21:58:26 +0330

    send otp

--- freelancer-frontend/src/features/authentication/SendOTPForm.jsx
+++ freelancer-frontend/src/features/authentication/SendOTPForm.jsx
@@ -1,22 +1,46 @@
-import { useState } from "react";
+import { useMutation } from "@tanstack/react-query";
+import { toast } from "react-hot-toast";
 
+import { getOtp } from "../../services/authService";
+
+import Loading from "../../ui/Loading";
 import TextField from "../../ui/TextField";
 
-const SendOTPForm = () => {
-  const [phoneNumber, setPhoneNumber] = useState("");
+const SendOTPForm = ({ phoneNumber, setStep, onChange }) => {
+  const { isPending, error, data, mutateAsync } = useMutation({
+    mutationFn: getOtp,
+  });
+
+  const sendOtpHandler = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await mutateAsync({ phoneNumber });
+      setStep(2);
+      toast.success(res?.message);
+    } catch (error) {
+      toast.error(error?.response?.data?.message);
+    }
+  };
 
   return (
     <div>
-      <form className="space-y-8">
+      <form onSubmit={sendOtpHandler} className="space-y-10">
         <div>
           <TextField
             label="Phone Number"
             name="phoneNumber"
             value={phoneNumber}
-            onChange={(e) => setPhoneNumber(e.target.value)}
+            onChange={onChange}
           />
         </div>
-        <button className="btn btn--primary w-full">Send Code</button>
+        {isPending ? (
+          <Loading />
+        ) : (
+          <button type="submit" className="btn btn--primary w-full">
+            Send Code
+          </button>
+        )}
       </form>
     </div>
   );
