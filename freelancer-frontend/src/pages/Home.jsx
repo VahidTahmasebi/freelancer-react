76842e42ab747acb30304c0504d75a223cd4ebfa
Vahid <vahid.tahmasebi95@gmail.com>
2023-12-02 17:04:43 +0330

    Add home and not found page

---
+++ freelancer-frontend/src/pages/Home.jsx
@@ -0,0 +1,8 @@
+function Home() {
+  return (
+    <div className="container xl:max-w-screen-xl">
+      <h1>Home Page</h1>
+    </div>
+  );
+}
+export default Home;
---
+++ freelancer-frontend/src/pages/NotFound.jsx
@@ -0,0 +1,22 @@
+import useMoveBack from "../hooks/useMoveBack";
+
+import { HiArrowRight } from "react-icons/hi";
+
+const NotFound = () => {
+  const moveBack = useMoveBack();
+  return (
+    <div className="sm:max-w-sm flex justify-center items-center pt-10">
+      <div>
+        <h1 className="mb-8 text-xl font-bold text-secondary-700">
+          The page you were looking for was not found
+        </h1>
+        <button onClick={moveBack}>
+          <HiArrowRight className="w-6 h-6 text-primary-900" />
+          <span>Back </span>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
