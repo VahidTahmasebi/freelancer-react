8c72cec732c7892fa855a072eaa61545294de198
Vahid <vahid.tahmasebi95@gmail.com>
2023-12-07 14:09:08 +0330

    Add date picker component

---
+++ freelancer-frontend/src/ui/DatePickerField.jsx
@@ -0,0 +1,23 @@
+import DatePicker from "react-multi-date-picker";
+import persian_fa from "react-date-object/locale/persian_fa";
+import persian from "react-date-object/calendars/persian";
+
+const DatePickerField = ({ label, setDate, date }) => {
+  return (
+    <div>
+      <span className="block mb-2 text-secondary-700">{label}</span>
+      <DatePicker
+        containerClassName="w-full"
+        inputClass="textField__input"
+        calendarPosition="bottom-center"
+        format="YYYY/MM/DD"
+        locale={persian}
+        calender={persian_fa}
+        value={date}
+        onChange={(date) => setDate(date)}
+      />
+    </div>
+  );
+};
+
+export default DatePickerField;
