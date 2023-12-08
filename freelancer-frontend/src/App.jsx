4ac22422835a8fd890ce74ebd48ff24c7a9d248a
Vahid <vahid.tahmasebi95@gmail.com>
2023-12-08 15:26:21 +0330

    Add dark mode syntax

--- freelancer-frontend/src/App.jsx
+++ freelancer-frontend/src/App.jsx
@@ -2,6 +2,8 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
 
+import { DarkModeProvider } from "./context/DarkModeContext";
+
 import Auth from "./pages/Auth";
 import CompleteProfile from "./pages/CompleteProfile";
 import AppLayout from "./ui/AppLayout";
@@ -15,22 +17,24 @@ const queryClient = new QueryClient();
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Toaster />
+    <DarkModeProvider>
+      <QueryClientProvider client={queryClient}>
+        <Toaster />
 
-      <Routes>
-        <Route path="/auth" element={<Auth />} />
-        <Route path="/complete-profile" element={<CompleteProfile />} />
-        <Route path="/owner" element={<AppLayout />}>
-          <Route index element={<Navigate to="dashboard" replace />} />
-          <Route path="dashboard" element={<OwnerDashboard />} />
-          <Route path="projects" element={<Projects />} />
-          <Route path="project:id" element={<Project />} />
-        </Route>
-        <Route path="/" element={<Home />} />
-        <Route path="/*" element={<NotFound />} />
-      </Routes>
-    </QueryClientProvider>
+        <Routes>
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/complete-profile" element={<CompleteProfile />} />
+          <Route path="/owner" element={<AppLayout />}>
+            <Route index element={<Navigate to="dashboard" replace />} />
+            <Route path="dashboard" element={<OwnerDashboard />} />
+            <Route path="projects" element={<Projects />} />
+            <Route path="project:id" element={<Project />} />
+          </Route>
+          <Route path="/" element={<Home />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+      </QueryClientProvider>
+    </DarkModeProvider>
   );
 }
 
