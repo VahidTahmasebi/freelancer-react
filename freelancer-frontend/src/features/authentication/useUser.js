b58ff72e080a89c1570951fd3f5db43fb0317404
Vahid <vahid.tahmasebi95@gmail.com>
2023-12-08 15:23:49 +0330

    Add user avatar to menu

--- freelancer-frontend/src/features/authentication/useUser.js
+++ freelancer-frontend/src/features/authentication/useUser.js
@@ -3,9 +3,13 @@ import { useQuery } from "@tanstack/react-query";
 import { getUser } from "../../services/authService";
 
 export default function useUser() {
-   return useQuery({
-    queryKey: ["get-user"],
+  const { isLoading, data } = useQuery({
+    queryKey: ["user"],
     queryFn: getUser,
     retry: false,
   });
+
+  const { user } = data || {};
+
+  return { isLoading, user };
 }
