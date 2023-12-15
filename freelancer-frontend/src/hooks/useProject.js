f18fe92db348f1b102db2439ac903010300d4ada
Vahid <vahid.tahmasebi95@gmail.com>
2023-12-15 13:10:56 +0330

    Get query string in project list

--- freelancer-frontend/src/hooks/useProject.js
+++ freelancer-frontend/src/hooks/useProject.js
@@ -1,11 +1,22 @@
+import { useLocation } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
+import { queryString } from "query-string";
 
 import { getProjectsApi } from "../services/projectService";
 
 export default function useProjects() {
+  const { search } = useLocation();
+  // convert query string to object
+  // The first method - best method
+  const queryObject = queryString.parse(search);
+
+  // The second method
+  // const queryObject = Object.fromEntries(new URLSearchParams(search));
+
   const { isLoading, data } = useQuery({
-    queryKey: ["projects"],
-    queryFn: getProjectsApi,
+    // You must update every time "queryObject" changes
+    queryKey: ["projects", queryObject],
+    queryFn: () => getProjectsApi(search),
   });
   const { projects } = data || {};
   return { isLoading, projects };
