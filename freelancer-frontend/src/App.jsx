7c1c9d5483a364630ebb6eb0f184ea547c68276e
Vahid <vahid.tahmasebi95@gmail.com>
2023-12-15 13:10:06 +0330

    Add query devtools

--- freelancer-frontend/src/App.jsx
+++ freelancer-frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "react-hot-toast";
 
 import { DarkModeProvider } from "./context/DarkModeContext";
@@ -23,6 +24,7 @@ function App() {
   return (
     <DarkModeProvider>
       <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
         <Toaster />
 
         <Routes>
